71de16ede42883ea03e1893d5d3f294ba6d0747d
csd0617 <2719669919@qq.com>
2024-11-28 13:32:09 +0800

    Site updated: 2024-11-28 13:32:09

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/csd1617/css/style.css",
   "/csd1617/js/script.js",
 ];
-const VERSION = "1732770801944";
+const VERSION = "1732771923406";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
