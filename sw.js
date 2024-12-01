5a9ef9e74334a089cc9471c982b4cd20e92510b0
csd0617 <2719669919@qq.com>
2024-12-01 23:52:04 +0800

    Site updated: 2024-12-01 23:52:04

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733068258757";
+const VERSION = "1733068318425";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
