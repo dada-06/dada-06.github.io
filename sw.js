abc4091d6b90b61c81fa5f4a11b36992742f4670
CSD0617 <2719669919@qq.com>
2025-04-01 21:28:42 +0800

    Site updated: 2025-04-01 21:28:42

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1743513215893";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1743514117864";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
