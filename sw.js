81d8a3b1c63b105d58d545b9f3b61082ec349a56
OneSword-LCLW <3457800744@qq.com>
2025-06-12 22:27:30 +0800

    Site updated: 2025-06-12 22:27:29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1749485245994";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1749738442904";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
