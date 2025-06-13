60ed925849bfe68abc2465ecfe98450c1e3dbafb
dada-06 <2719669919@qq.com>
2025-06-13 19:06:14 +0800

    Site updated: 2025-06-13 19:06:13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1749812525491";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1749812768613";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
